e5315b1987749e7453da706a7ec61ee5e1e62519
Ryan nguyen <ryan.ngn@outlook.com>
2024-03-08 02:12:53 +0000

    create route handler for get all videos, get video info, and post video

--- routes/videos.js
+++ routes/videos.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const router = express.Router();
-const { getAllVideosWithoutComments, getVideoInfo } = require('../src/helper')
+const { getAllVideosWithoutComments, getVideoInfo, uploadVideo } = require('../src/helper')
 
 //routes handlers
-router.route('/').get((req,res) => {
+router.route('/').get((req, res) => {
     res.json(getAllVideosWithoutComments())
 })
 
+router.route('/').post((req, res) => {
+    try {
+        const video = uploadVideo(req.body);
+        res.status(201).json(video);
+    } catch (error) {
+        res.status(500).send(`Couldn't upload video: ${error}`);
+    }
+})
+
 router.route('/:id')
-    .get((req,res) => res.json(getVideoInfo(req.params.id)))
+    .get((req, res) => res.json(getVideoInfo(req.params.id)))
+
 // export at the end of file
 module.exports = router;
